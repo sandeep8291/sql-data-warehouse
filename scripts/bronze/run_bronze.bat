5455518440d64199a2da678d531bdb7fced72619
sandeep yadav <155004834+sandeep8291@users.noreply.github.com>
2025-06-08 11:49:38 +0530

    Add files via upload

---
+++ scripts/bronze/run_bronze.bat
@@ -0,0 +1,15 @@
+@echo off
+REM ===============================
+REM Batch Script to Load Bronze Layer in PostgreSQL
+REM Uses psql to execute your SQL file with \COPY commands
+REM ===============================
+
+echo Starting bronze layer load using PSQL...
+
+REM --- Set your database credentials and path here ---
+SET PGPASSWORD=12345678
+psql -U postgres -d datawarehouse -h localhost < "C:\Users\sande\OneDrive\Documents\copy_load.sql"
+
+echo.
+echo Load complete. Press any key to exit.
+pause
