f0d9ef6a1f14976e1041b37e0da6102f045906fa
sandeep yadav <155004834+sandeep8291@users.noreply.github.com>
2025-06-08 11:52:43 +0530

    Update run_bronze.bat

--- scripts/bronze/run_bronze.bat
+++ scripts/bronze/run_bronze.bat
@@ -8,7 +8,7 @@ echo Starting bronze layer load using PSQL...
 
 REM --- Set your database credentials and path here ---
 SET PGPASSWORD= ex.12345679
-psql -U postgres -d datawarehouse -h localhost < "C:\Users\sande\OneDrive\Documents\copy_load.sql"
+psql -U postgres -d datawarehouse -h localhost < your_sql_file_path
 
 echo.
 echo Load complete. Press any key to exit.
